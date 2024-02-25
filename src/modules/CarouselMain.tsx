fca299d0e56014d9cfbd36f67a1eb89ff77bd642
SergeyBogomolovv <bogomolovs693@gmail.com>
2024-02-25 19:17:27 +0300

    added banner

--- src/modules/CarouselMain.tsx
+++ src/modules/CarouselMain.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Carousel } from 'antd'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
+import { Link } from 'react-router-dom'
 
 const CarouselMain: React.FC = () => {
   return (
-    <Carousel autoplay autoplaySpeed={3000}>
-      <AspectRatio ratio={16 / 9}>
-        <img src='./1.jpeg' alt='' />
-      </AspectRatio>
+    <Carousel autoplay autoplaySpeed={5000}>
+      <Link to='/Жемчуг'>
+        <AspectRatio ratio={16 / 9}>
+          <img src='./banner1.png' alt='' />
+        </AspectRatio>
+      </Link>
       <AspectRatio ratio={16 / 9}>
         <img src='./2911395.jpg' alt='' />
+      </AspectRatio>
+      <AspectRatio ratio={16 / 9}>
+        <img src='./1.jpeg' alt='' />
       </AspectRatio>
     </Carousel>
   )
