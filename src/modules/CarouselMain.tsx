aa4e8fd2ace319e8d7fe3d3abdda498ee2c2b222
SergeyBogomolovv <bogomolovs693@gmail.com>
2024-02-27 18:37:16 +0300

    dfg

--- src/modules/CarouselMain.tsx
+++ src/modules/CarouselMain.tsx
@@ -4,14 +4,20 @@ import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { Link } from 'react-router-dom'
 
 const CarouselMain: React.FC = () => {
+  const ration = window.innerWidth > 900 ? 16 / 6.2 : 16 / 9
+
   return (
     <Carousel autoplay autoplaySpeed={5000}>
       <Link to='/Жемчуг'>
-        <AspectRatio ratio={16 / 6.2}>
-          <img src='./banner.png' alt='' className='w-full' />
+        <AspectRatio ratio={ration}>
+          <img
+            src={window.innerWidth > 900 ? './banner.png' : './banner1.png'}
+            alt=''
+            className='w-full'
+          />
         </AspectRatio>
       </Link>
-      <AspectRatio ratio={16 / 6.2}>
+      <AspectRatio ratio={ration}>
         <img src='./1.jpeg' alt='' className='w-full' />
       </AspectRatio>
     </Carousel>
