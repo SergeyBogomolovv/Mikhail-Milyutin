6b0cd257c3faa8858d1addb26dc49c81598d11dd
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-07-26 15:12:18 +0300

    changed fon

--- src/collections/index.ts
+++ src/collections/index.ts
@@ -13,57 +13,68 @@ import { tradicii } from "./tradicii";
 
 export const collections: ICollection[] = [
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/alica/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/alicafon.jpg",
     title: "Алиса в стране чудес",
     cards: alica,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/grani/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/granifon.jpg",
     title: "Грани",
     cards: grani,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/olga/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/olgafon.jpg",
     title: "Ольга",
     cards: olga,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/gzhel/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/gzhelfon.jpg",
     title: "Коллекция в стиле Гжель",
     cards: gzhel,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/classic/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/classicfon.jpg",
     title: "Классика",
     cards: classic,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/mosty/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/mostyfon.jpg",
     title: "Знаменитые мосты и решетки",
     cards: mosty,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/pinap/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/pinapfon.jpg",
     title: "Пин-ап",
     cards: pinap,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/newyear/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/newyearfon.jpg",
     title: "Новогоднее настроение",
     cards: newyear,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/flowers/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/flowersfon.jpg",
     title: "Сад цветов",
     cards: flowers,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/iskusstvo/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/iskusstvofon.jpg",
     title: "Предметы искусства",
     cards: iskusstvo,
   },
   {
-    image: "https://storage.yandexcloud.net/mikhail-milutin/tradicii/fon.png",
+    image:
+      "https://storage.yandexcloud.net/mikhail-milutin/preview/tradiciifon.jpg",
     title: "Сохраняя традиции",
     cards: tradicii,
   },
